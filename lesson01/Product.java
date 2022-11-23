d6f66b5469826edf90f690be9738778ed226a47d
dtnfktu <vtlg@mail.ru>
2022-11-23 09:26:36 +0300

    added from seminar 1

--- lesson01/Product.java
+++ lesson01/Product.java
@@ -1,3 +1,34 @@
-class Product{
-    
+package lesson01;
+
+public class Product {
+    private String name;
+    private Double price;
+
+    public Product(String name, Double price) {
+        this(name);
+        this.price = price;
+    }
+
+    public Product(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double value) {
+        if (value <= 0) {
+            return;
+        }
+        this.price = value;
+    }
+    @Override
+    public String toString() {
+        return String.format("%s %,.2f", this.name, this.price );
+    }
 }
